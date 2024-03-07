ddc327dbc30b30629983ea669418d7a497584871
LordNinth <rr621801@gmail.com>
2024-03-07 04:42:03 +0000

    forgot dom manipulation :(

--- app.js
+++ app.js
@@ -1,28 +1,40 @@
 const recruitBtn = document.querySelector(".btn");
 const recruitBox = document.querySelector(".imgSrc");
+const inventory = document.querySelector(".inventory");
+const newUl = document.querySelector(".inventoryList");
 
 function recruitManager() {
   let brilliantBlessing = Math.random().toFixed(2);
   if (brilliantBlessing > 0.7) {
-    recruitBox.imgsrc = `./assets/images/${imgFiveArray[Math.floor(Math.random()*imgFiveArray.length)]}`;
-    console.log(recruitBox.src);
+    recruitBox.src = `./assets/images/${
+      imgFiveArray[Math.floor(Math.random() * imgFiveArray.length)]
+    }`;
+    return recruitBox.src;
   } else {
-    recruitBox.src = `./assets/images/${imgFourArray[Math.floor(Math.random()*imgFourArray.length)]}`;
-    console.log(recruitBox.src);
+    recruitBox.src = `./assets/images/${
+      imgFourArray[Math.floor(Math.random() * imgFourArray.length)]
+    }`;
+    return recruitBox.src;
   }
 }
 
 const imgFiveArray = [
   "five1.jpg",
   "five2.jpg",
-  "five3,jpg",
+  "five3.jpg",
   "five4.jpg",
   "five5.jpg",
 ];
 const imgFourArray = ["four1.jpg", "four2.jpg", "four3.jpg"];
 
-
+function addToInventory() {
+  const newLi = document.createElement("LI");
+  newLi.innerText = `${recruitManager()}`;
+  newUl.appendChild(newLi);
+}
 
 recruitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
   recruitManager();
+  addToInventory();
 });
