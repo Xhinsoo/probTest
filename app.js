b7d43926eb590adfcc399917786b88c8d642746d
LordNinth <rr621801@gmail.com>
2024-03-09 03:55:34 +0000

    probabilityTest

--- app.js
+++ app.js
@@ -2,20 +2,23 @@ const recruitBtn = document.querySelector(".btn");
 const recruitBox = document.querySelector(".imgSrc");
 const inventory = document.querySelector(".inventory");
 const newUl = document.querySelector(".inventoryList");
+const probBtn = document.querySelector(".probButton");
 
 //recruit manager ()
 function recruitManager() {
   let brilliantBlessing = Math.random().toFixed(2);
-  if (brilliantBlessing > 0.7) {
-    recruitBox.src = `./assets/images/${
-      imgFiveArray[Math.floor(Math.random() * imgFiveArray.length)]
-    }`;
-    return recruitBox.src;
+
+  let fiveIndex = imgFiveArray[Math.floor(Math.random() * imgFiveArray.length)];
+  let fourIndex = imgFourArray[Math.floor(Math.random() * imgFourArray.length)];
+
+  if (brilliantBlessing > 0.74) {
+    recruitBox.src = `./assets/images/${fiveIndex}`;
+
+    return fiveIndex;
   } else {
-    recruitBox.src = `./assets/images/${
-      imgFourArray[Math.floor(Math.random() * imgFourArray.length)]
-    }`;
-    return recruitBox.src;
+    recruitBox.src = `./assets/images/${fourIndex}`;
+
+    return fourIndex;
   }
 }
 //array of recruits
@@ -28,18 +31,33 @@ const imgFiveArray = [
 ];
 const imgFourArray = ["four1.jpg", "four2.jpg", "four3.jpg"];
 
-
 //push to inv
 function addToInventory() {
-  const newLi = document.createElement("LI");
+  const newLi = document.createElement("li");
+  newLi.classList.add("list");
   newLi.innerText = `${recruitManager()}`;
   newUl.appendChild(newLi);
 }
-
+//tally
+function probabilityChecker() {
+  const newUlLength = document
+    .querySelector(".inventoryList")
+    .getElementsByTagName("li");
+  let counter = 0;
+  for (i = 0; i < newUlLength.length; i++) {
+    console.log(newUlLength[i].innerText);
+    counter++;
+    console.log(counter);
+  }
+}
 
 //execute 2 ()
 recruitBtn.addEventListener("click", (e) => {
   e.preventDefault();
   recruitManager();
   addToInventory();
+});
+
+probBtn.addEventListener("click", (e) => {
+  probabilityChecker();
 });
